a9a3c6e47d29c26444e7eb854cfe88a339e334ba
hatatori <kcireh.i@gmail.com>
2018-02-16 16:06:21 -0200

    Create js.js

---
+++ js.js
@@ -0,0 +1,23 @@
+window.onload=function(){
+
+	t = document.querySelectorAll("textarea")
+	console.log(t)
+	for(i=0;i<t.length;i++){
+
+		if(typeof(t[i]) != "undefined")
+			format(t[i])
+		t[i].onkeyup=function(){
+			format(this)
+		}
+	}	
+
+	function format(ele){
+		ele.setAttribute("spellcheck","false")
+		val = ele.value;
+		val.replace(/</g,"&lt;").replace(">","&gt;")
+		cod = ele.parentElement.querySelector("code");
+		ele.parentElement.querySelector("code").innerHTML = val;
+		Prism.highlightElement(cod);
+	}
+
+}
